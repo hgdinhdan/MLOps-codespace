2d4ccdf5be36f03bcc091fcee96a32a994ec951a
DanEE2 <hgdinhdan22082001@gmail.com>
2024-12-16 00:22:57 +0700

    setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version = "0.0.0",
-    author="DanHoang",
+    author="Hoang Dinh Dan",
     author_email="hgdinhdan22082001@gmail.com",
     packages= find_packages()
 )
